1c111a9faf093c2513901b049e0f77a260799025
Chinnu-2005 <gkrvkoushik@gmail.com>
2025-04-06 19:36:49 +0530

    initial project

---
+++ assets/js/script.js
@@ -0,0 +1,8 @@
+
+const preload=document.querySelector('[data-preload]');
+
+window.addEventListener("load",function(){
+    preload.classList.add('loaded');
+    document.body.classList.add('loaded');
+}
+);
